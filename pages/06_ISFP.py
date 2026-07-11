b7d52871d4fa1309fa499e8fe5b471fbf57b34e8
raunlee0311-bit <raunlee0311@gmail.com>
2026-07-11 11:54:10 +0900

    Create 06_ISFP.py

---
+++ pages/06_ISFP.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title("🧠 ISFP - 호기심 많은 예술가")
+
+st.subheader("성격 특징")
+st.write("""
+ISFP는 따뜻하고 감성적인 성향을 가진 유형입니다.
+자신만의 개성과 창의성을 중요하게 생각합니다.
+""")
+
+st.subheader("✨ 장점")
+st.markdown("""
+- 예술적 감각
+- 배려심
+- 유연한 사고
+""")
+
+st.subheader("💼 추천 직업")
+st.write("디자이너, 예술가, 사진작가, 작가")
