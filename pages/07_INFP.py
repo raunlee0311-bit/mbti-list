1e5811e69c3568c84383836fb55ea1740a149dc9
raunlee0311-bit <raunlee0311@gmail.com>
2026-07-11 11:54:36 +0900

    Create 07_INFP.py

---
+++ pages/07_INFP.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title("🧠 INFP - 열정적인 중재자")
+
+st.subheader("성격 특징")
+st.write("""
+INFP는 이상과 가치관을 중요하게 생각하는 유형입니다.
+상상력이 풍부하고 감수성이 뛰어납니다.
+""")
+
+st.subheader("✨ 장점")
+st.markdown("""
+- 창의성
+- 공감 능력
+- 상상력
+""")
+
+st.subheader("💼 추천 직업")
+st.write("작가, 상담사, 디자이너, 예술가")
