71b1f4df8940d2803957dab8f353c6fe272e82f2
raunlee0311-bit <raunlee0311@gmail.com>
2026-07-11 11:49:44 +0900

    Create 01_ISTJ.py

---
+++ 01_ISTJ.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.title("🧠 ISTJ - 청렴결백한 논리주의자")
+
+st.subheader("성격 특징")
+st.write("""
+ISTJ는 책임감이 강하고 계획적인 성향을 가지고 있습니다.
+맡은 일을 끝까지 수행하며 규칙과 질서를 중요하게 생각합니다.
+""")
+
+st.subheader("✨ 장점")
+st.markdown("""
+- 책임감이 강함
+- 꼼꼼하고 성실함
+- 신뢰할 수 있음
+""")
+
+st.subheader("💼 추천 직업")
+st.write("""
+공무원, 회계사, 경찰, 관리자, 분석가
+""")
