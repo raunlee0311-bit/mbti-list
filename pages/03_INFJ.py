7181330441e4fd34a379435221b8f3d684864554
raunlee0311-bit <raunlee0311@gmail.com>
2026-07-11 11:52:35 +0900

    Create 03_INFJ.py

---
+++ pages/03_INFJ.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title("🧠 INFJ - 선의의 옹호자")
+
+st.subheader("성격 특징")
+st.write("""
+INFJ는 깊은 생각과 높은 공감 능력을 가진 유형입니다.
+사람들의 마음을 잘 이해하고 의미 있는 목표를 추구합니다.
+""")
+
+st.subheader("✨ 장점")
+st.markdown("""
+- 뛰어난 공감 능력
+- 창의적인 사고
+- 깊은 통찰력
+""")
+
+st.subheader("💼 추천 직업")
+st.write("심리상담사, 작가, 연구원, 기획자")
