4e6faa677cfd8a2c2a7d2380aca93064f65b4318
raunlee0311-bit <raunlee0311@gmail.com>
2026-07-11 11:51:09 +0900

    Create 02_ISFJ.py

---
+++ 02_ISFJ.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.title("🧠 ISFJ - 용감한 수호자")
+
+st.subheader("성격 특징")
+st.write("""
+ISFJ는 따뜻하고 배려심이 많으며
+주변 사람들을 잘 챙기는 성격입니다.
+""")
+
+st.subheader("✨ 장점")
+st.markdown("""
+- 책임감
+- 친절함
+- 협동심
+""")
+
+st.subheader("💼 추천 직업")
+st.write("""
+교사, 간호사, 상담사, 사회복지사
+""")
