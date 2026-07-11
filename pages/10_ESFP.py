8414a2b735ce1dd5e192a59942b7cb27ade71f18
raunlee0311-bit <raunlee0311@gmail.com>
2026-07-11 11:56:08 +0900

    Create 10_ESFP.py

---
+++ pages/10_ESFP.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("🧠 ESFP - 자유로운 연예인")
+
+st.subheader("성격 특징")
+st.write("""
+ESFP는 밝고 에너지가 넘치며 사람들과 함께하는 것을 좋아합니다.
+""")
+
+st.subheader("✨ 장점")
+st.markdown("""
+- 긍정적인 성격
+- 친화력
+- 표현력
+""")
+
+st.subheader("💼 추천 직업")
+st.write("연예인, 이벤트 기획자, 서비스 전문가")
